189bb8036f765b848dec0d7f1c7de64e12b0c1e1
Munawwar khan <92468595+Munawwar0@users.noreply.github.com>
2022-10-14 22:49:56 +0530

    Update Find_Binod.py

--- Binod/Find_Binod.py
+++ Binod/Find_Binod.py
@@ -14,13 +14,13 @@ def isBinod(filename):
 
 for item in dir_contents :
     if item.endswith('txt'):
-        print (f'\nDetecting Binod in {item}')
+        print (f'\nDetecting_Binod_in_{item}')
         flag = isBinod(item)
         if flag:
-            print(f'Binod found in {item}')
+            print(f'Binod_found_in_{item}')
             nBinod += 1
         else :
-            print(f'Binod is not found in {item}')
+            print(f'Binod_is_not_found_in_{item}')
         
-print("\n****Find Binod Summary****")
+print("\n****Find_Binod_Summary****")
 print(f'{nBinod} files found with Binod into it!!!')
